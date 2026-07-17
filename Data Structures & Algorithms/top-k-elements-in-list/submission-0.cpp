9dfa2f21f162ea481ea83f2120382e183b534512
VinuthaST-vincy <vinuthastvincy@gmail.com>
2026-07-17 11:16:35 +0530

    Add logic to find top k elements in a list
    Implement frequency counting and min-heap for top k elements.

--- Data Structures & Algorithms/top-k-elements-in-list/submission-0.cpp
+++ Data Structures & Algorithms/top-k-elements-in-list/submission-0.cpp
@@ -6,16 +6,21 @@ public:
         for(int i=0;i<nums.size();i++){
             freq[nums[i]]++;
         }
+        //after getting its frequency, now to return the top, there is a need of some sorting to faster accessing the k top. 
+        //taking as minheap cuz, i need the top ele, so the smallest sitting at the top which means it is getting ready to be evicted.
+        
         priority_queue<pair<int, int>, vector<pair<int, int>>, greater<pair<int, int>>> minheap;
         for(auto& p:freq){
+            //p.second, cuz by default heap compares pair of first by default, and we rank be frequency so p.second at pair of first.
             minheap.push({p.second, p.first});
+            //when top k is exceded, which means i need top 2 so 3>2 i will evacuate that 3rd top.
             if(minheap.size()>k){
                 minheap.pop();
             }
         }
-
         vector<int> result;
         while(!minheap.empty()){
+            //keep adding minheap.top element to the result so at next when poped the top 2nd remains in minheap, repeat for the same and keeps storing until minheap gets empty which are the top shortlisted one.
             result.push_back(minheap.top().second);
             minheap.pop();
         }
