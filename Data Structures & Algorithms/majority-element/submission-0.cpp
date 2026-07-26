7231fe8956a79e9fdd8f3bc3dec1a45695e97c2b
VinuthaST-vincy <vinuthastvincy@gmail.com>
2026-07-26 07:12:27 +0530

    Add: majority-element - submission-0

---
+++ Data Structures & Algorithms/majority-element/submission-0.cpp
@@ -0,0 +1,15 @@
+class Solution {
+public:
+    int majorityElement(vector<int>& nums) {
+       unordered_map<int, int> freq;
+       for(int i=0;i<nums.size();i++){
+           freq[nums[i]]++;
+       }
+       for(auto& p:freq){
+        if(p.second>nums.size()/2){
+            return p.first;
+        }
+       }
+       return {};
+    }
+};
